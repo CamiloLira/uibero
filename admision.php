92089634e875ac16ee86ee4b916eb10a944f0b37
camilolira <c.liramunizaga@gmail.com>
2015-11-03 09:53:05 -0300

    css colores y división Header

--- admision.php
+++ admision.php
@@ -6,18 +6,8 @@
 	<link rel="stylesheet" type="text/css" href="css/admision.css">
 </head>
 <body>
-	<header>
-		<figure>
-			<img class="logo" src="">
-		</figure>
-		<nav>
-		  <ul>	
-			<li>Facultades</li>
-			<li>Requisitos</li>
-			<li>Contacto</li>
-		  </ul>
-		</nav>
-	</header>
+	<!-- header -->
+	<?php include("header.php"); ?>
 
 
 </body>
---
+++ header.php
@@ -0,0 +1,12 @@
+<header>
+		<figure>
+			<img class="logo" src="">
+		</figure>
+		<nav>
+		  <ul>	
+			<li>Facultades</li>
+			<li>Requisitos</li>
+			<li>Contacto</li>
+		  </ul>
+		</nav>
+	</header>
