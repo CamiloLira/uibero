18261b57a6afa9a8ebec5b94c24bb7ae1b8ad109
camilolira <c.liramunizaga@gmail.com>
2015-11-03 10:43:29 -0300

    changes

--- admision.php
+++ admision.php
@@ -3,7 +3,12 @@
 	<?php include("header.php"); ?>
 <body>
 
-
+	<div class="bienvenido">
+		<p>Bienvenido a ser lo que quieres</p>
+		
+			<img class="admicionbarra" src="images/admicionbarra.png">
+	
+	</div>
 
 </body>
 	<?php include("footer.php"); ?>
