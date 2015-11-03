1ae9522a4bd02800192715859f61e00438f59455
camilolira <c.liramunizaga@gmail.com>
2015-11-03 12:45:06 -0300

    posicionado

--- admision.php
+++ admision.php
@@ -14,6 +14,28 @@
 	<div class="container-slider">
 		
 	</div>
+	<!-- cierre Container Slider -->
+	<!-- barra facultadades -->
+		<figure>
+			<img class="barrafacultades" src="images/nuestras.png">
+		</figure>
+	<!-- facultades -->
+	<div class="facultades">
+		
+		<!-- facultad educacion hexagono-->
+		<div class="facultad-educacion">
+			<img src="images/rojo.png">
+			<a href=""><p class="rojo titulos-facultad">FACULTAD DE CIENCIAS 
+				Y EDUCACIÓN</p><a/>
+			<div class="mover-carreras-facultad">
+				<p class="texto-facultades">Pedagogía en Ingles</p>
+				<p class="texto-facultades">Pedagogía en Matemáticas y Fisica</p>
+			</div>
+		</div>
+
+		
+			
+	</div>
 
 </body>
 	<?php include("footer.php"); ?>
