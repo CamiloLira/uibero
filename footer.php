aa3d4454804450c1781100f64579d0a8d1d9e1e1
camilolira <c.liramunizaga@gmail.com>
2015-11-04 14:44:31 -0300

    llogos footer

--- footer.php
+++ footer.php
@@ -1,7 +1,12 @@
 <footer>
-<div id="prefooter" class="bordes2"></div>
+
+<div id="prefooter" class="bordes2">
+	<img class="acreditacion" src="images/acreditacion.png" width="250px"/>
+	<!-- img footer-->
+	<img class="logo-footer" src="images/ibero2.png" width="386px"/>
+</div>
 <div id="footersimple">
-	<img class="acreditacion" src="images/acreditacion.png">
+	
 </div>
 
 </footer>
