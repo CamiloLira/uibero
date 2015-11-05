f8416e387a9ae920458a992968c98ee49362b72d
camilolira <c.liramunizaga@gmail.com>
2015-11-04 22:51:44 -0300

    footoer

--- facultad-ingles.php
+++ facultad-ingles.php
@@ -21,16 +21,17 @@
 	<div class="seccion-uno">
 		<h1 class="gris-oscuro">Pedagogía en Ingles</h1>
 		<div class="introduccion">
-		<p class="ingles-introduccion gris-oscuro">
-			Contamos con un staff académico altamente especializado 
-			lo que nos permite formar docentes con un dominio competente
-			 del inglés, preparados didáctica y pedagógicamente para crear
-			  condiciones y procesos favorables al aprendizaje de la lengua 
-			  inglesa, en contextos variados y para una población estudiantil 
-			  heterogénea.
-			Para el ejercicio de sus prácticas profesionales, la carrera cuenta 
-			con un Centro de Experimentación, Investigación y Prácticas Pedagógicas: “Los Andes Country Day College”, ubicado en la comuna de Peñalolén y además mantiene convenios  con los Liceos Emblemáticos de la comuna de Santiago.
-		</p>
+			<h4>Introducción</h4>
+			<p class="ingles-introduccion gris-oscuro">
+				Contamos con un staff académico altamente especializado 
+				lo que nos permite formar docentes con un dominio competente
+				 del inglés, preparados didáctica y pedagógicamente para crear
+				  condiciones y procesos favorables al aprendizaje de la lengua 
+				  inglesa, en contextos variados y para una población estudiantil 
+				  heterogénea.
+				Para el ejercicio de sus prácticas profesionales, la carrera cuenta 
+				con un Centro de Experimentación, Investigación y Prácticas Pedagógicas: “Los Andes Country Day College”, ubicado en la comuna de Peñalolén y además mantiene convenios  con los Liceos Emblemáticos de la comuna de Santiago.
+			</p>
 		</div>
 	</div>
 
--- footer.php
+++ footer.php
@@ -1,9 +1,9 @@
 <footer>
 
 <div id="prefooter" class="bordes2">
-	<img class="acreditacion" src="images/acreditacion.png" width="250px"/>
+	<img class="acreditacion" src="images/acreditacion.png" width="15%"/>
 	<!-- img footer-->
-	<img class="logo-footer" src="images/ibero2.png" width="386px"/>
+	<img class="logo-footer" src="images/ibero2.png" width="30%"/>
 </div>
 <div id="footersimple">
 	
