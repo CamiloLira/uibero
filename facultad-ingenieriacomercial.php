4214acacad7abfc9bf26109ddbebede3e01dac0d
camilolira <c.liramunizaga@gmail.com>
2015-11-05 02:22:53 -0300

    posicionaminoi

---
+++ facultad-ingenieriacomercial.php
@@ -0,0 +1,96 @@
+
+<!-- header -->
+<?php include("header.php"); ?>
+<body>
+
+
+	<div class="bienvenidos">
+		<p class="azul">Bienvenido a ser lo que quieres</p>
+		
+		<img class="admicionbarra" src="images/admicionbarra.png">
+	</div>
+
+	<!-- Barra educacion -->
+    <div class="vineta-educacion">
+		<img src="images/vineta-ingenieria.png">
+		<h2 class="titulo-facultad-educacion blanco">Facultad de Ciencias y Educación</h2>
+    </div>
+	
+<!-- caja seccion 1  -->
+
+	<div class="seccion-uno-comercial">
+		<h1 class="gris-oscuro">Ingeniería Comercial</h1>
+		<div class="introduccion-comercial">
+			<h4 class="gris-oscuro">Introducción</h4>
+			<p class="comercial-introduccion gris-oscuro">
+				¿Te gusta innovar, emprender y 
+				liderar equipos de trabajo? Entonces ven e infó
+				rmate de la carrera de Ingeniería Comercial que imparte la Univ
+				ersidad Iberoamericana. Aquí podrás acceder a una carrera con orientación p
+				ráctica en negocios, donde aprenderás sobre finanzas, marketing y recursos humanos. 
+				 Country Day College”, ubicado en la comuna de Peñalolén y 
+				 además mantiene convenios  con los Liceos Emblemáticos de la comuna de Santiago.
+			</p>
+		</div>
+		
+		<div class="informacion-directora-comercial">
+			<img class="acredita-comercial" src="images/3acredita.png">
+			<img src="http://lorempixel.com/310/190/">
+			<h4 class="director-carrera-comercial gris-oscuro">Directora de Carrera </h4>
+			<p class="gris">Mg. Georg Spee Gaona</p>
+			<p class="gris">E-mail: george.spee@uibero.cl</p>
+			<p class="gris">Teléfono: +56 2 23899082.</p>
+
+		</div>
+
+	</div>
+
+	<div class="perfil-egresado-comercial">
+	 <h4 class="gris-oscuro">Perfil Egresado</h4>
+	 <br>
+	 <p class="gris-oscuro">
+	 	Es un profesional que está capacitado para liderar empresas y 
+	 	organizaciones, ya que su formación está orientada a la administración y 
+	 	gestión de empresas. Posee conocimientos en marketing, finanzas, recursos humanos, 
+	 	economía y tecnologías de la información.
+	 </p>
+	</div>
+
+<!-- cajaseccion 2 -->
+
+ <div class="seccion-dos-comercial">
+  	<div class="campo-comercial">
+  		<h4 class="gris-oscuro">Empleabilidad / Campo Laboral</h4>
+  		<br>
+  		<p class="gris-oscuro">De acuerdo a datos del Mineduc, la empleabilidad de la carrera de
+  		 Ingeniería Comercial es de un 92% en el primer año tras la titulación, y de un 94,8% en el segundo.
+El Ingeniero Comercial de la Universidad Iberoamericana posee alta capacidad de emprendimiento y competencias
+ para desempeñarse en marketing, finanzas, recursos humanos, economía y tecnologías de la información.
+		</p>
+		<a href="" class="morado descarga-malla-comercial">DESCARGA TU MALLA<a/>
+  	</div>
+
+  	<!-- info general comercial-->
+  		<div class="info-general-comercial">
+  				<h4 class="gris-oscuro">Información General</h4>
+  					<br>
+					<p class="gris-oscuro">Título Profesional </p>
+					<p class="gris-oscuro">Ingeniero Comercial  </p>
+					<p class="gris-oscuro">Grado Académico</p>
+					<p class="gris-oscuro">Licenciado en Ciencias en </p>
+					<p class="gris-oscuro">la Administración de Empresas.</p>
+					<p class="gris-oscuro">Duración</p>
+					<p class="gris-oscuro">10 semestres</p>
+					<br>
+					<p class="gris-oscuro">Régimen: Diurno</p>
+  		</div>
+
+ </div>
+
+
+					
+<!-- div para los bordes -->
+<div></div>
+	
+	</body>
+									
