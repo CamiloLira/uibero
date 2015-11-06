6c906c6b7681b7f26477f913f7a195bf00d090ba
camilolira <c.liramunizaga@gmail.com>
2015-11-06 04:57:29 -0300

    clima dominio jaja

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 <footer>
 <img class="logo-footer" src="images/logofooter.png" />
 
-<div class="texto-footer"><p>Casa Central, Padre Miguel de Olivares Nº1620, Santiago. </p><p>Fono: (02) 23899000</p><p> E-mail: admision@uibero.clIr</p></div>
+<div class="texto-footer"><p>Casa Central, Padre Miguel de Olivares Nº1620, Santiago. </p><p>Fono: (02) 23899000</p><p> E-mail: admision@uibero.cl</p></div>
 <img class="acrefooter" src="images/acrefooter.png" />
 </footer>
 </body>
