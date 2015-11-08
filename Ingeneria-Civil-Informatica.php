d517bcdfc12f439e18f101f37a9cf4315ed6563e
sylviamelgarejo <sylvia.cc7@gmail.com>
2015-11-08 18:02:33 -0300

    civil-iformatica

---
+++ Ingeneria-Civil-Informatica.php
@@ -0,0 +1,87 @@
+
+<!-- header -->
+<?php include("header.php"); ?>
+<body>
+
+
+	<div class="bienvenidos">
+		<p class="azul">Bienvenido a ser lo que quieres</p>
+		<a href="https://www.facebook.com/uiberoamericanacl" target="_blank"><div class="facebook-barra-admision"></div></a>
+		<a href="http://www.twitter.com/Admision_Uibero" target="_blank"><div class="twitter-barra-admision"></div></a>
+		<a href="http://www.instagram.com/admisionuibero" target="_blank"><div class="instagram-barra-admision"></div></a>
+		<img class="admicionbarra" src="images/admicionbarra.png">
+	</div>
+
+	<!-- Barra educacion -->
+    <div class="vineta-educacion">
+		<img src="images/vineta-ingenieria.png">
+		<h2 class="titulo-facultad-educacion blanco">Facultad de Ciencias de la Ingeniería y Administración</h2>
+    </div>
+	
+<!-- caja seccion 1  -->
+
+	<div class="seccion-uno-fingenieria">
+		<h1 class="gris-oscuro">Ingeneria Civil Informatica</h1>
+		<div class="introduccion-fingenieria">
+			<h4 class="gris-oscuro">Introducción</h4>
+			<p class="fingenieria-introduccion gris-oscuro">
+La Ingeniería en Ejecución Electrónica que se imparte en la Universidad Iberoamericana de Ciencias y Tecnología, es una carrera que está orientada a formar a profesionales de excelencia que serán capaces de enfrentar y resolver problemas complejos de la ingeniería. Para ello, cuentan con docentes con una amplia experiencia laboral, quienes los instruyen en Ciencias Básicas y asignaturas pre-profesionales. 
+
+			</p>
+		</div>
+		
+		<div class="informacion-directora-fingenieria">
+			<!--<img class="acredita-fingenieria" src="images/3acredita.png">-->
+			<img src="http://lorempixel.com/310/190/">
+			<h4 class="director-carrera-fingenieria gris-oscuro">Director de Carrera </h4>
+			<p class="gris-oscuro">Ing. José Miguel Santibáñez A.</p>
+			<p class="gris-oscuro">E-mail: jose.santibanez@uibero.cl</p>
+			<p class="gris-oscuro">Teléfono: +56 223899032.</p>
+
+		</div>
+
+	</div>
+
+	<div class="perfil-egresado-fingenieria-ejecucion-mover">
+	 <h4 class="gris-oscuro">Perfil Egresado</h4>
+	 <br>
+	 <p class="gris-oscuro">
+El egresado de Ingeniería en Ejecución Electrónica es un profesional  formado en la búsqueda de la excelencia y con alto sentido social, gracias al énfasis de prácticas profesionales en el ámbito de una cultura universidad-empresa. Junto con ello, posee las competencias para desempeñarse en la operación y mantención de los sistemas de su especialidad, como también en la supervisión y ejecución de proyectos en las áreas de Telecomunicaciones e Informática o de Automatización y Robótica.
+	 </p>
+	</div>
+
+<!-- cajaseccion 2 -->
+
+ <div class="seccion-dos-fingenieria">
+  	<div class="campo-fingenieria">
+  		<h4 class="gris-oscuro">Empleabilidad / Campo Laboral</h4>
+  		<br>
+  		<p class="gris-oscuro">Los ingenieros de Ejecución Electrónica pueden trabajar administrando sistemas electrónicos en áreas de Telecomunicaciones o de Automatización y Robótica, en empresas de consultoría, servicios y ventas. También pueden desarrollar sus propios emprendimientos de éxito en el área de servicios electrónicos.
+		</p>
+		<a href="" class="morado descarga-malla-fingenieria-ejecucioncivil">DESCARGA TU MALLA<a/>
+  	</div>
+
+  	<!-- info general comercial-->
+  		<div class="info-general-fingenieria-electronica">
+  				<h4 class="gris-oscuro">Información General</h4>
+  					<br>
+					<p class="gris-oscuro">Título Profesional </p>
+					<p class="gris-oscuro">Ingeniero de Ejecución Electronica</p>
+					<p class="gris-oscuro">Grado Académico</p>
+					<p class="gris-oscuro">Licenciado en Ciencias de la Ingeniería</p>
+					<p class="gris-oscuro">Duración</p>
+					<p class="gris-oscuro">8 semestres</p>
+					<br>
+					<p class="gris-oscuro">Régimen: Vespertino</p>
+  		</div>
+
+ </div>
+
+
+					
+<!-- div para los bordes -->
+<div></div>
+	
+<?php include("footer.php"); ?>
+						
+			
