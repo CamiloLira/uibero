46448b77d89634c6713d27c96b0d007ad966df0e
sylviamelgarejo <sylvia.cc7@gmail.com>
2015-11-10 22:26:21 -0300

    footer

--- footer2.php
+++ footer2.php
@@ -1,11 +1,11 @@
-<div id="footer-admision”>
+<div id="footeradmision">
 <div class="triangulo_inf"><img class="logo-footer" src="images/logofooter.png" /></div>
 <div class="triangulo_blanco"></div>
 <div class="triangulo_azul"></div>
 <div class="footer2" >
 
 
-<div class="texto-footer"><p>Casa Central, Padre Miguel de Olivares Nº1620, Santiago. </p><p>Fono: (02) 23899000</p><p> E-mail: admision@uibero.cl</p></div>
+<div class="texto-footer"><p>Casa Central, Padre Miguel de Olivares N¼1620, Santiago. </p><p>Fono: (02) 23899000</p><p> E-mail: admision@uibero.cl</p></div>
 <img class="acrefooter" src="images/acrefooter.png" />
 </div>
 </div>
